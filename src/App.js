1291cff9b0dae0832472a0d07a09512a3eb86943
ChuxiZhang <55220214+chuxi-z@users.noreply.github.com>
2020-11-08 12:27:33 -0500

    init repo

--- src/App.js
+++ src/App.js
@@ -3,21 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div id="game">
+      <h1>Tic Tac Toe</h1>
+      <div id="board">
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+        <button class="cell">
+          <span class="cell-content"></span>
+        </button>
+      </div>
+    </div>
+
+
+    <div id="modal-overlay">
+      <div id="game-result-modal">
+        <div id="result-container">
+          <div id="winner-container">
+            <span></span>
+          </div>
+        </div>
+        <div id="new-game-container">
+          <button id="new-game-button">Start New Game</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
